711d5f57fc58d698103db98b7f1661fd9b6c5e8b
pola1497 <topola_14@hotmail.com>
2025-11-20 19:58:27 -0600

    feat: complete task CRUD (create, edit, delete)

--- src/app/tasks/task-form/task-form.component.ts
+++ src/app/tasks/task-form/task-form.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { TasksService, TaskPayload } from '../tasks.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-task-form',
@@ -10,13 +11,17 @@ import { TasksService, TaskPayload } from '../tasks.service';
   templateUrl: './task-form.component.html',
   styleUrl: './task-form.component.scss'
 })
-export class TaskFormComponent {
+export class TaskFormComponent implements OnInit {
 
   taskForm: FormGroup;
 
+  isEditMode = false;
+  taskId?: number | null = null;
+
   constructor(
     private fb: FormBuilder,
-    private tasksService: TasksService
+    private tasksService: TasksService,
+    private route: ActivatedRoute
   ) {
     this.taskForm = this.fb.group({
       title: ['', Validators.required],
@@ -27,21 +32,71 @@ export class TaskFormComponent {
     });
   }
 
+  ngOnInit(): void {
+    const idParam = this.route.snapshot.paramMap.get('id');
+
+    if (idParam) {
+      this.isEditMode = true;
+      this.taskId = Number(idParam);
+      console.log('Edition mode form', this.taskId);
+      this.tasksService.getTaskById(this.taskId).subscribe({
+        next: (task) => {
+          this.taskForm.patchValue({
+            title: task.title,
+            description: task.description,
+            status: task.status,
+            priority: task.priority,
+            dueDate: task.dueDate ? task.dueDate.split('T')[0] : ''
+          });
+        },
+        error: (error) => {
+          console.error('Error fetching task:', error);
+        }
+      });
+    } else {
+      this.isEditMode = false;
+      this.taskId = null;
+      console.log('Creation mode form');
+    }
+  }
+
   onSubmit() {
     if (this.taskForm.invalid) {
       console.log('Not valid form');
       return;
     }
 
-    const payload = this.taskForm.value;
-    this.tasksService.createTask(payload).subscribe({
-      next: (taskCreated) => {
-        console.log('Task created successfully:', taskCreated);
-      },
-      error: (error) => {
-        console.error('Error creating task:', error);
-      }
-    });
+    const formValue = this.taskForm.value;
+
+    const payload: TaskPayload = {
+      title: formValue.title,
+      description: formValue.description || undefined,
+      status: formValue.status,
+      priority: formValue.priority,
+      dueDate: formValue.dueDate
+        ? new Date(formValue.dueDate).toISOString()
+        : undefined,
+    };
+
+    if (this.isEditMode && this.taskId !== null) {
+      this.tasksService.updateTask(Number(this.taskId), payload).subscribe({
+        next: (updatedTask) => {
+          console.log('Task updated successfully:', updatedTask);
+        },
+        error: (error) => {
+          console.error('Error updating task:', error);
+        }
+      });
+    } else {
+      this.tasksService.createTask(payload).subscribe({
+        next: (createdTask) => {
+          console.log('Task created successfully:', createdTask);
+        },
+        error: (error) => {
+          console.error('Error creating task:', error);
+        }
+      });
+    }
   }
 }
 
